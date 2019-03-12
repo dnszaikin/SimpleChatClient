1385a16f9068d4e613583aab2876b247418d5f7b
Denis Zaikin <dnszaikin@hotmail.com>
2019-03-12 19:50:59 +0300

    changes

--- SimpleChatClient/Program.cs
+++ SimpleChatClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,56 @@ namespace SimpleChatClient
 {
     class Program
     {
+        // TCPClient example https://docs.microsoft.com/ru-ru/dotnet/api/system.net.sockets.tcpclient?view=netframework-4.7.2
+        static void MyChatClient(String server, Int32 port, String message)
+        {
+            try
+            {
+                TcpClient client = new TcpClient(server, port);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+
         static void Main(string[] args)
         {
+            MyChatClient("localhost", 8181, "Hi chat! I'm a new user!");
         }
     }
 }
